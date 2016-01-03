42d69fe7d0f2208297d18de937b223d6b568dee8
Haley <hhawes@rapiddg.com>
2016-01-03 11:27:42 -0500

    Responsive refinements

--- sites/all/themes/zen/js/scripts.js
+++ sites/all/themes/zen/js/scripts.js
@@ -1,8 +1,11 @@
 jQuery(document).ready(function($){
 
-	var background = $('.view-top-banner img').attr('src');
-	$('.view-top-banner').css('background-image', 'url(' + background + ')');
-	$('.breadcrumb').prependTo('.view-top-banner .views-row');
+	var bannerExists = $('.view-top-banner').length;
+	if (bannerExists) {
+		var background = $('.view-top-banner img').attr('src');
+		$('.view-top-banner').css('background-image', 'url(' + background + ')');
+		$('.breadcrumb').prependTo('.view-top-banner .views-row');
+	}
 
 	$('#toggle-nav').click(function() {
 		$('body').toggleClass('show-nav');
@@ -25,27 +28,29 @@ jQuery(document).ready(function($){
 	    responsive: true,
 	});
 
-	 // Resize Window
-    $(window).resize(function () {
-        if (this.resizeTO) clearTimeout(this.resizeTO);
-        this.resizeTO = setTimeout(function () {
-            $(this).trigger('resizeEnd');
-        }, 100);
-    });
+	 // // Resize Window
+  //   $(window).resize(function () {
+  //       if (this.resizeTO) clearTimeout(this.resizeTO);
+  //       this.resizeTO = setTimeout(function () {
+  //           $(this).trigger('resizeEnd');
+  //       }, 100);
+  //   });
 
-    //Hide parallax containers for mobile
-    $(window).bind('resizeEnd', function () {
-        var width = $(window).width();
-        // var isMobile = ((/Android|iPad|iPhone|iPod|BlackBerry|Windows Phone/i).test(navigator.userAgent || navigator.vendor || window.opera) || width <= 599);
-        var isMobile = (width <= 767);
-        var carouselExists = $('.owl-carousel').length;
-        if (isMobile && carouselExists) {
-        	var itemHeight = $('.view-homepage-slides .owl-wrapper').height();
-            $('.view-homepage-slides .owl-item').css('height', itemHeight);
-        }
-        else {
-        	$('.view-homepage-slides .owl-item').css('height', 'auto');
-        }
-    }).trigger('resizeEnd');
+  //   //Hide parallax containers for mobile
+  //   $(window).bind('resizeEnd', function () {
+  //       var width = $(window).width();
+  //       // var isMobile = ((/Android|iPad|iPhone|iPod|BlackBerry|Windows Phone/i).test(navigator.userAgent || navigator.vendor || window.opera) || width <= 599);
+  //       var isMobile = (width <= 767);
+  //       var carouselExists = $('.owl-carousel').length;
+  //       if (isMobile && carouselExists) {
+  //       	$(window).load(function() {
+  //       		var itemHeight = $('.view-homepage-slides .owl-wrapper').height();
+  //           	$('.view-homepage-slides .owl-item').css('height', itemHeight);
+  //       	});
+  //       }
+  //       else {
+  //       	$('.view-homepage-slides .owl-item').css('height', 'auto');
+  //       }
+  //   }).trigger('resizeEnd');
 
 });
